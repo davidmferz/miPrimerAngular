b5952c9efa38792066d69ba1c6c7dc17b7a59d9b
davidmferz <capricornio_sol1@hotmail.com>
2025-07-08 09:10:58 -0600

    08-jun

--- src/app/layout/main-layaout/main-layaout.ts
+++ src/app/layout/main-layaout/main-layaout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layaout',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './main-layaout.html',
-  styleUrl: './main-layaout.css'
+  styleUrl: './main-layaout.css',
 })
-export class MainLayaout {
-
-}
+export class MainLayaout {}
